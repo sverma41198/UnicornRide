ccdf36e82032417a407f2d4b1c1386ca47e2eb87
Sourabh <sverma@msystechnologies.com>
2023-05-12 10:02:21 +0000

    Added resourses to the Local Repository

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_dwvMjSPe6', 
+        userPoolClientId: '75a5cgguiimsbhotdt6901au8i',
+        region: 'ap-south-1'
+    },
+    api: {
+        invokeUrl: 'https://zp86ojmw7f.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
